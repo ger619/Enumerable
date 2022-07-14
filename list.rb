038ae26b1973d55f339ac4e75cb7ced837041891
Dinma-Faith <phaytep@gmail.com>
2022-07-14 19:41:16 +0100

    Write test to verify solution

--- list.rb
+++ list.rb
@@ -9,3 +9,17 @@ class MyList
     @list.each(&block)
   end
 end
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+list.all? {|e| e < 5}
+list.all? {|e| e > 5}
+
+# Test #any?
+list.any? {|e| e == 2}
+list.any? {|e| e == 5}
+
+# Test #filter
+list.filter {|e| e.even?}
